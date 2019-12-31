72368abd5c93fdd05781641619bf9482c4956f86
Bryan C Walsh <Walsh.C.Bryan@Gmail.com>
2019-12-30 19:00:45 -0500

    regex 2

--- math.py
+++ math.py
@@ -70,3 +70,15 @@ def is_allowed_specific_char(string):
 
 print(is_allowed_specific_char("ABCDEFabcdef123450"))
 print(is_allowed_specific_char("*&%@#!}{"))
+#Write a Python program that matches a string that has an a followed by zero or more b's.
+
+def text_match(text):
+    patterns = 'ab*?'
+    if re.search(patterns,  text):
+        return 'Found a match!'
+    else:
+        return('Not matched!')
+
+print(text_match("ac"))
+print(text_match("abc"))
+print(text_match("abbc"))
