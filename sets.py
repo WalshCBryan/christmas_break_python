5d06781be6fa23392d8b6cecfe64e54622c059b4
Bryan C Walsh <Walsh.C.Bryan@Gmail.com>
2020-01-02 22:35:53 -0500

    add, remove, update, and discard

--- sets.py
+++ sets.py
@@ -8,4 +8,24 @@ print(n)
 for num in n:
     print(num)
 
+n.add(5)
+
+print(n)
+
+n.remove(5)
+
+print(n)
+
+n.update([5,6,7])
+
+print(n)
+
+n.discard(9)
+
+print(n)
+
+n.discard(5)
+
+print(n)
+
 
